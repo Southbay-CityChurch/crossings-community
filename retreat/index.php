4f5151da08a76e22131653fe964b147a771304ef
Eric Ritchey <eric@eandjdesign.com>
2013-09-17 00:14:10 -0700

    crossings retreat website, take 1

--- retreat/index.php
+++ retreat/index.php
@@ -1,31 +1,71 @@
-<?php
-   $inline_style = "
-   h1 {
-      margin:0;
-   }
-   
-   #footer {
-      height:216px;
-      overflow:hidden;
-      background-image:url('images/footer_home.gif')
-   }
-   
-   #navigation {
-      top:330px;
-      left:445px;
-   }
-   #navigation li.home {
-      display:none;
-   }
-   
-   "
-?>
-
-<?php require '_top.php'; ?>
-<h1 class="home"><span>Crossings Community Beach Retreat 2007</span></h1>
-
-<p class="hide">Behold, how good and how pleasant it is for brethren to dwell in unity<br />Psalm 133:1</p>
-
-<?php require '_nav.php'; ?>
-
-<?php require '_bottom.php'; ?>
+<!doctype html>
+<html>
+	<head>
+		<title>Crossings Tahoe Retreat <?= date('Y'); ?></title>
+		<style type="text/css" media="screen">
+			body {
+				font-family: "Helvetica Neue";
+				line-height: 1.75;
+				font-size: 120%;
+			}
+			h1, h2, h3, h4, h5, h6 {
+				margin-bottom: 0;
+				border-bottom: 1px solid #ccc;
+			}
+			h1 + p,
+			h2 + p,
+			h3 + p,
+			h4 + p,
+			h5 + p,
+			h6 + p,
+			h1 + ol,
+			h2 + ol,
+			h3 + ol,
+			h4 + ol,
+			h5 + ol,
+			h6 + ol {
+				margin-top: 0;
+			}
+			#contain {
+				margin: auto;
+				max-width: 800px;
+			}
+			#footer {
+				margin: auto;
+				text-align: center;
+				font-size: 60%;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="contain">
+			<h1><span>Crossings Tahoe Retreat</span></h1>
+			<p><strong>&ldquo;&hellip;praying at all times in the Spirit, with all prayer and supplication. To that end keep alert with all perseverance, making supplication for all the saints&hellip;&rdquo;<br />Ephesians 6:18 ESV</strong></p>
+			<h3>Directions to the Main House</h3>
+			<ol>
+				<li>From Campbell, head <strong>north</strong> on <strong>CA-17/I-880</strong>. Exit onto <strong>Mission Blvd</strong> toward Sacramento/I-680. Merge onto <strong>I-680 N toward Sacramento</strong>.</li>
+				<li>Continue on I-680 to the <strong>Benicia-Martinez Bridge</strong>. You'll need $5 in cash for the toll.</li>
+				<li>Continue on I-680 for 13 more miles. Keep <strong>right at the fork</strong> and merge onto <strong>I-80 E</strong>. Stay on I-80 for about 40 more miles.</li>
+				<li>Continue on <strong>I-80Bus E/US-50 E/Capital City Freeway</strong> toward South Lake Tahoe. After 5 miles, stay on US-50 E.</li>
+				<li>After about 95 miles, turn <strong>right</strong> on <strong>Pioneer Trail</strong>. In another 8 miles, turn <strong>right</strong> on <strong>US-50 E/Lake Tahoe Blvd</strong>, and head on into Nevada. </li>
+				<li>Turn <strong>right</strong> on <strong>Kingsbury Grade Road</strong>.</li>
+				<li>Turn <strong>right</strong> on <strong>S Benjamin Drive</strong>.</li>
+				<li>Turn <strong>right</strong> onto <strong>Jack Drive</strong>. The house will be on the left in about a half of a mile.</li>
+			</ol>
+			<p><a href="https://www.google.com/maps/preview#!q=661+Jack+Dr%2C+Stateline%2C+NV+89449" title="Google Maps Directions">661 Jack Drive, Stateline, NV 89449</a></p>
+			<div id="footer">
+				<p class="copyright small"><?= date('Y'); ?> Crossings Community Church</p>
+			</div>
+		</div>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-2468518-1']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+	</body>
+</html>
