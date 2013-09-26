5fa97dcf040321c2d4da4e18310b20ce902667c9
Eric Ritchey <eric@eandjdesign.com>
2013-09-26 14:31:44 -0700

    retreat changes

--- retreat/index.php
+++ retreat/index.php
@@ -2,15 +2,26 @@
 <html>
 	<head>
 		<title>Crossings Tahoe Retreat <?= date('Y'); ?></title>
+		<meta name="viewport" content="width=device-width">
 		<style type="text/css" media="screen">
 			body {
 				font-family: "Helvetica Neue";
-				line-height: 1.75;
+				line-height: 1.5;
 				font-size: 120%;
 			}
 			h1, h2, h3, h4, h5, h6 {
+				clear: both;
 				margin-bottom: 0;
 				border-bottom: 1px solid #ccc;
+				text-align: center;
+			}
+			ol,
+			ul {
+				margin: 0;
+				padding: 0;
+			}
+			li {
+				margin-bottom: .75rem;
 			}
 			h1 + p,
 			h2 + p,
@@ -23,36 +34,191 @@
 			h3 + ol,
 			h4 + ol,
 			h5 + ol,
-			h6 + ol {
-				margin-top: 0;
-			}
+			h6 + ol { margin-top: 0; }
 			#contain {
-				margin: auto;
+				margin: 1rem auto;
 				max-width: 800px;
+				padding: 0 1rem;
+			}
+			section {
+				margin-bottom: 1rem;
+				overflow: hidden;
+				clear: both;
 			}
 			#footer {
+				clear: both;
 				margin: auto;
 				text-align: center;
 				font-size: 60%;
+			}
+			.scripture .reference {
+				display: block;
+				text-align: right;
+			}
+			.directions-link {
+				text-align: center;
+			}
+			.contacts ul,
+			.house-assignments ul,
+			.schedule ol {
+				margin: 0;
+				padding: 0;
+			}
+			.contacts li,
+			.house-assignments li,
+			.schedule li {
+				margin: 0;
+				padding: 0;
+				list-style: none;
+			}
+			.contacts li span {
+				min-width: 90px;
+				display: inline-block;
+			}
+			.house-assignments li {
+				width: 33.3%;
+				float: left;
+			}
+			.directions li {
+				margin-left: 1.75rem;
+			}
+			.schedule li {
+				margin-bottom: .75rem;
 			}
 		</style>
 	</head>
 	<body>
 		<div id="contain">
-			<h1><span>Crossings Tahoe Retreat</span></h1>
-			<p><strong>&ldquo;&hellip;praying at all times in the Spirit, with all prayer and supplication. To that end keep alert with all perseverance, making supplication for all the saints&hellip;&rdquo;<br />Ephesians 6:18 ESV</strong></p>
-			<h3>Directions to the Main House</h3>
-			<ol>
-				<li>From Campbell, head <strong>north</strong> on <strong>CA-17/I-880</strong>. Exit onto <strong>Mission Blvd</strong> toward Sacramento/I-680. Merge onto <strong>I-680 N toward Sacramento</strong>.</li>
-				<li>Continue on I-680 to the <strong>Benicia-Martinez Bridge</strong>. You'll need $5 in cash for the toll.</li>
-				<li>Continue on I-680 for 13 more miles. Keep <strong>right at the fork</strong> and merge onto <strong>I-80 E</strong>. Stay on I-80 for about 40 more miles.</li>
-				<li>Continue on <strong>I-80Bus E/US-50 E/Capital City Freeway</strong> toward South Lake Tahoe. After 5 miles, stay on US-50 E.</li>
-				<li>After about 95 miles, turn <strong>right</strong> on <strong>Pioneer Trail</strong>. In another 8 miles, turn <strong>right</strong> on <strong>US-50 E/Lake Tahoe Blvd</strong>, and head on into Nevada. </li>
-				<li>Turn <strong>right</strong> on <strong>Kingsbury Grade Road</strong>.</li>
-				<li>Turn <strong>right</strong> on <strong>S Benjamin Drive</strong>.</li>
-				<li>Turn <strong>right</strong> onto <strong>Jack Drive</strong>. The house will be on the left in about a half of a mile.</li>
-			</ol>
-			<p><a href="https://www.google.com/maps/preview#!q=661+Jack+Dr%2C+Stateline%2C+NV+89449" title="Google Maps Directions">661 Jack Drive, Stateline, NV 89449</a></p>
+			<section class="directions">
+				<h1><span>Crossings Tahoe Retreat</span></h1>
+				<p class="scripture"><em>&ldquo;&hellip;praying at all times in the Spirit, with all prayer and supplication. To that end keep alert with all perseverance, making supplication for all the saints&hellip;&rdquo;</em><br /><span class="reference">Ephesians 6:18 ESV</span></p>
+				<h3>To the Main House</h3>
+				<p class="directions-link"><a href="https://www.google.com/maps/preview#!q=661+Jack+Dr%2C+Stateline%2C+NV+89449" title="Google Maps Directions">661 Jack Drive, Stateline, NV 89449</a></p>
+				<ol>
+					<li>From Campbell, head north on CA-17/I-880. Exit onto Mission Blvd toward Sacramento/I-680. Merge onto I-680 N toward Sacramento.</li>
+					<li>Continue on I-680 to the Benicia-Martinez Bridge. You'll need $5 in cash for the toll.</li>
+					<li>Continue on I-680 for 13 more miles. Keep right at the fork and merge onto I-80 E. Stay on I-80 for about 40 more miles.</li>
+					<li>Continue on I-80Bus E/US-50 E/Capital City Freeway toward South Lake Tahoe. After 5 miles, stay on US-50 E.</li>
+					<li>After about 95 miles, turn right on Pioneer Trail. In another 8 miles, turn right on US-50 E/Lake Tahoe Blvd, and head on into Nevada. </li>
+					<li>Turn right on Kingsbury Grade Road.</li>
+					<li>Turn right on S Benjamin Drive.</li>
+					<li>Turn right onto Jack Drive. The house will be on the left in about a half of a mile.</li>
+				</ol>
+			</section>
+			<section class="contacts">
+				<h1><span>Contacts</span></h1>
+				<p>If you are having trouble, try calling one of us:</p>
+				<ul>
+					<li><span>Eric</span> (408) 340-3564</li>
+					<li><span>Jenny R</span> (408) 340-3013</li>
+					<li><span>Aaron</span> (408) 410-2052</li>
+					<li><span>Brianna</span> (408) 930-7350</li>
+					<li><span>Trevor</span> (408) 807-3003</li>
+					<li><span>Jen Z</span> (408) 410-4526</li>
+				</ul>
+				<p>Cell reception might be spotty in the hills, so you could email <script type="text/javascript">
+//<![CDATA[
+<!--
+var x="function f(x){var i,o=\"\",l=x.length;for(i=l-1;i>=0;i--) {try{o+=x.c" +
+"harAt(i);}catch(e){}}return o;}f(\")\\\"function f(x,y){var i,o=\\\"\\\\\\\""+
+"\\\\,l=x.length;for(i=0;i<l;i++){if(i==119)y+=i;y%=127;o+=String.fromCharCo" +
+"de(x.charCodeAt(i)^(y++));}return o;}f(\\\"\\\\\\\\\\\\023\\\\\\\\027\\\\\\" +
+"\\032\\\\\\\\017\\\\\\\\026\\\\\\\\031\\\\\\\\023\\\\\\\\n.vpjp`ji +6j,e|jv" +
+",N1yt\\\\\\\\177{lv qyspf`SKWGMC^NHGB@T\\\\\\\\000L_\\\\\\\\\\\\\\\\n\\\\\\" +
+"\\021\\\\\\\\024A_CT\\\\\\\\\\\\\\\\\\\\\\\\007g\\\\\\\\036xS^)-b\\\\\\\\t!" +
+"+(>\\\\\\\\024kt\\\\\\\\001)# 6l~3mv|mgc\\\"\\\\,119)\\\"(f};)lo,0(rtsbus.o" +
+" nruter};)i(tArahc.x=+o{)--i;0=>i;1-l=i(rof}}{)e(hctac};l=+l;x=+x{yrt{)29=!" +
+")31/l(tAedoCrahc.x(elihw;lo=l,htgnel.x=lo,\\\"\\\"=o,i rav{)x(f noitcnuf\")" ;
+while(x=eval(x));
+//-->
+//]]>
+</script>
+ or <script type="text/javascript">
+//<![CDATA[
+<!--
+var x="function f(x){var i,o=\"\",l=x.length;for(i=0;i<l;i+=2) {if(i+1<l)o+=" +
+"x.charAt(i+1);try{o+=x.charAt(i);}catch(e){}}return o;}f(\"ufcnitnof x({)av" +
+" r,i=o\\\"\\\"o,=l.xelgnhtl,o=;lhwli(e.xhcraoCedtAl(1/)3=!84{)rt{y+xx=l;=+;" +
+"lc}tahce({)}}of(r=i-l;1>i0=i;--{)+ox=c.ahAr(t)i};erutnro s.buts(r,0lo;)f}\\" +
+"\"(3),7\\\"\\\\GC\\\\M\\\\\\\\V\\\\3M02\\\\\\\\L^16\\\\00\\\\00\\\\\\\\03\\" +
+"\\0r\\\\\\\\\\\\02\\\\00\\\\03\\\\\\\\V+:E04\\\\0n\\\\\\\\\\\\\\\\r3\\\\00\\"+
+"\\\\\\10\\\\02\\\\02\\\\\\\\35\\\\0~\\\\51|s-U0Rgi`~'|Y$li/a7l02\\\\\\\\34\\"+
+"\\01\\\\02\\\\\\\\34\\\\0:\\\\13\\\\05\\\\03\\\\\\\\04\\\\07\\\\02\\\\\\\\0" +
+"7\\\\02\\\\02\\\\\\\\27\\\\03\\\\02\\\\\\\\37\\\\06\\\\03\\\\\\\\16\\\\07\\" +
+"\\00\\\\\\\\37\\\\06\\\\01\\\\\\\\5Q00\\\\\\\\35\\\\04\\\\00\\\\\\\\16\\\\0" +
+"7\\\\00\\\\\\\\10\\\\0F\\\\_?07\\\\05\\\\00\\\\\\\\6-=}xg6q3;=!%!77\\\\1$\\" +
+"\\+!9 %(\\\\-(\\\"}fo;n uret}r);+)y+^(i)t(eAodrCha.c(xdeCoarChomfrg.intr=So" +
+"+7;12%=;y=iy+3)=7i=f({i+)i+l;i<0;i=r(foh;gten.l=x,l\\\"\\\\\\\"\\\\o=i,r va" +
+"){,y(x fontincfu)\\\"\")"                                                    ;
+while(x=eval(x));
+//-->
+//]]>
+</script>
+ to get a hold of us too.</p>
+			</section>
+			<section class="house-assignments">
+				<h1><span>Housing</span></h1>
+				<h2>Main House</h2>
+				<ul>
+					<li>Bujuklian</li>
+					<li>Carleton</li>
+					<li>Frank</li>
+					<li>Fraser</li>
+					<li>Krista</li>
+					<li>Plett</li>
+					<li>Ritchey</li>
+					<li>Russo</li>
+					<li>Shea</li>
+					<li>Zamora</li>
+				</ul>
+				<h2>Zink House</h2>
+				<ul>
+					<li>Goulding</li>
+					<li>Miranda</li>
+					<li>Scianna</li>
+					<li>Zink</li>
+				</ul>
+			</section>
+			<section class="schedule">
+				<h1><span>The Schedule</span></h1>
+				<ol>
+					<li><strong>Friday Dinner 5:30pm</strong><br>Session 1 8-9:30pm</li>
+					<li><strong>Saturday Breakfast (Continental 7-8:30am)</strong><br>Session 2 9:30-11am</li>
+					<li><strong>Lunch 12pm</strong></li>
+					<li>Kids Craft Time 3pm (optional)</li>
+					<li><strong>Saturday Dinner 5:30pm</strong><br>Session 3 8-9:30pm</li>
+					<li><strong>Sunday Breakfast (Continental 7-8:30am)</strong><br>Session 4 9:30-11am</li>
+					<li><strong>Quick Lunch 11:15pm</strong></li>
+				</ol>
+			</section>
+			<section class="notes">
+				<h1><span>Notes</span></h1>
+				<ul>
+					<li>If you'll need any specific tool for a meal, make sure to bring it! There's no way to know what will be available at the main house.</li>
+					<li>Please remember to only bring refrigerated items during your designated time frame since there are only 2 refrigerators for the whole group.</li>
+					<li>Do not leave any leftover food from your meal after the retreat is concluded.</li>
+					<li>Please be considerate with your portion size when serving up food. Wait on seconds until everyone has had a chance to eat.</li>
+					<li>If you are on "meal duty", you are also on garbage and clean up detail. Please locate the garbage bags and remove them.</li>
+					<li>If you are signed up for a "helper" spot, please remember to maintain that detail throughout the weekend.</li>
+					<li>For those coming up early, meals won't be provided until Friday, so plan accordingly. There will be a pizza night on Thursday for those arriving in time.</li>
+					<li>Cooking and baking are very different at high altitude! <a href="http://www.kingarthurflour.com/recipe/high-altitude-baking.html" target="_blank">See this link</a> about how to adjust your recipes. Keep in mind that our cabin is located at 7200ft above sea level.</li>
+				</ul>
+			</section>
+			<section class="what-to-bring">
+				<h1><span>What to Bring</span></h1>
+				<ul>
+					<li>Snacks!</li>
+					<li>Games-board, card, arcade&hellip;it's your choice</li>
+					<li>Extra towels-unless you really like to share</li>
+					<li>Baby Monitors-since there aren't any babysitters for the evening meetings</li>
+					<li>Clothes for colder weather</li>
+					<li>Swimsuits if you want to go in the pool or jacuzzi</li>
+				</ul>
+			</section>
+			<section class="forecast">
+				<iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="http://forecast.io/embed/#lat=38.968597&lon=-119.891596&name=Stateline, Nevada"> </iframe>
+			</section>
+
 			<div id="footer">
 				<p class="copyright small"><?= date('Y'); ?> Crossings Community Church</p>
 			</div>
